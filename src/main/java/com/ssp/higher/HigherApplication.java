4223c1f3fb5006a1e6be4a7d47b627dc002d171c
zouyu <344805956@qq.com>
2019-08-08 11:43:41 +0800

    getmeapple

---
+++ src/main/java/com/ssp/higher/HigherApplication.java
@@ -0,0 +1,13 @@
+package com.ssp.higher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HigherApplication.class, args);
+	}
+
+}
